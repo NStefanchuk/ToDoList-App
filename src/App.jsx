30ea01977d4c7069601d0d8a6ecb5473b2f9c9cc
Nikita Stefanchuk <nik.stefanchuk@gmail.com>
2025-09-01 17:58:00 +0200

    project set up

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <div className="text-3xl font-bold underline">Hello Tailwind!</div>
+    </div>
+  )
+}
+
+export default App
