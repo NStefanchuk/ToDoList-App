c4c22c27ee80c37c1d3f9ce7aad82359be5ad532
Nikita Stefanchuk <nik.stefanchuk@gmail.com>
2025-09-02 17:00:23 +0200

    logic

--- src/components/TodoItems.jsx
+++ src/components/TodoItems.jsx
@@ -1,20 +1,34 @@
 import React from 'react'
-import {
-  FaTrash, // delete
-  FaTrashAlt,
-  FaRegCircle, // not tick
-  FaCheckCircle, // tick
-  FaCalendarPlus, // todo/calendar plus
-} from 'react-icons/fa'
+import { FaTrashAlt, FaRegCircle, FaCheckCircle } from 'react-icons/fa'
 
-const TodoItems = () => {
+const TodoItems = ({ text, id, isComplete, deleteTodo, toggle }) => {
   return (
     <div className="flex items-center my-3 gap-2">
-      <div className='flex flex-1 items-center cursor-pointer'>
-        <FaCheckCircle className="text-2xl text-black-500 w-7" />
-        <p className='text-slate-700 ml-4 text-[17px]'>Learn Coding</p>
+      <div
+        onClick={() => {
+          toggle(id)
+        }}
+        className="flex flex-1 items-center cursor-pointer"
+      >
+        {isComplete ? (
+          <FaCheckCircle className="text-2xl text-green-500 w-7" />
+        ) : (
+          <FaRegCircle className="text-2xl text-gray-500 w-7" />
+        )}
+        <p
+          className={`text-slate-700 ml-4 text-[17px] decoration-slate-500 ${
+            isComplete ? 'line-through' : ''
+          }`}
+        >
+          {text}
+        </p>
       </div>
-      <FaTrashAlt className="text-2xl text-black-500 w-3.5 cursor-pointer" />
+      <FaTrashAlt
+        onClick={() => {
+          deleteTodo(id)
+        }}
+        className="text-2xl text-black-500 w-3.5 cursor-pointer"
+      />
     </div>
   )
 }
